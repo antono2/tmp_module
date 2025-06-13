09d23d0648a62b99e910dddbbbcc374daa5f302f
antono2 <antono2@web.de>
2025-06-13 14:06:33 +0200

    Ticket: run from wrong working dir

--- another_module/another_module.v
+++ another_module/another_module.v
@@ -1,3 +1,11 @@
 module another_module
 
+import tmp_module
+
 pub const v_modroot = @VMODROOT
+
+pub struct MyChildStruct{
+pub mut:
+  parent_struct tmp_module.MyParentStruct
+}
+
--- src/tmp.c.v
+++ src/tmp.c.v
@@ -1,3 +1,9 @@
 module tmp_module
 
 pub const v_modroot = @VMODROOT
+
+pub struct MyParentStruct{
+pub mut:
+  value int
+}
+
